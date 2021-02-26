43ebf395e87158c7a1d4434ef12b9210d02bf7f6
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-26 02:41:19 -0500

    Servicio de usuarios CREADO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }from '@angular/forms';
 import { routing, appRoutingProviders } from './app.routing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -17,7 +18,8 @@ import { RegisterComponent } from './components/register/register.component';
   imports: [
     BrowserModule,
     routing,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     appRoutingProviders
