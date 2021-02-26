0175f044d444e76e336b76e51721aa767d7ea74f
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-26 01:03:59 -0500

    Pagina y formulario de registro CREADO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }from '@angular/forms';
 import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule
   ],
   providers: [
     appRoutingProviders
