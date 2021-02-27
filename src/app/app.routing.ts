40acdcaed1f527a7c0611f1c6a42b7d0deddcc1c
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-26 22:16:08 -0500

    Componente HOME y menu de USUARIO

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,9 +2,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 const appRoutes: Routes = [
-    { path: '', component: LoginComponent },
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent},
     { path: 'registro', component: RegisterComponent}
 ];
