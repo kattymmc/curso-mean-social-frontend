e0607a574bbdf4d0b5ed19c1e8056c6b7717f712
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-27 14:42:51 -0500

    Metodo updateUser() y mensajes de erro

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -32,5 +32,25 @@ export class UserEditComponent implements OnInit{
 
     onSubmit(){
         console.log(this.user);
+        this._userService.updateUser(this.user).subscribe(
+            response =>{
+                if(!response.user){
+                    this.status = 'error'
+                }else{
+                    this.status = 'success';
+                    // Actualiza el localStorage
+                    localStorage.setItem('identity', JSON.stringify(this.user));
+                    // Actualiza el valor de identity de esta clase
+                    this.identity = this.user;
+                }
+            }, error => {
+                var errorMessage = <any>error;
+                console.log(errorMessage);
+
+                if(errorMessage != null){
+                    this.status = 'error'
+                }
+            }
+        );
     }
 }
