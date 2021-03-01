7732997bcf0ee08e5d77aefe1e69c2d129e22292
Katty <katherine.miraval@unmsm.edu.pe>
2021-03-01 14:40:39 -0500

    users-component, router y template CREADO

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+import { GLOBAL } from '../../services/global';
+
+@Component({
+    selector: 'users',
+    templateUrl: './users.component.html',
+    providers: [UserService]
+})
+export class UsersComponent implements OnInit{
+    public title: string;
+    public identity;
+    public token;
+    public status: string;
+
+    constructor(
+        private _route: ActivatedRoute,
+        private _router: Router,
+        private _userService: UserService,
+    ){
+        this.title = 'Gente';
+        this.identity = this._userService.getIdentity();
+        this.token = this._userService.getToken();
+    }
+
+    ngOnInit(){
+        console.log('user.component cargado');
+    }
+}
